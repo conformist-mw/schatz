abe5368a830b3e564448645169a5983b256bccb9
conformist-mw <conformist.mw@gmail.com>
2017-05-12 15:25:03 +0300

    access to gallery by albums, tags

--- dachshunds/schatz/views.py
+++ dachshunds/schatz/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Album, Photo
+
+
+class GalleryAlbums(ListView):
+    model = Album
+    context_object_name = 'albums'
+
+
+class PhotoGallery(ListView):
+    model = Photo
+    context_object_name = 'photos'
+
+    def get_queryset(self, *args, **kwargs):
+        album_id = Album.objects.filter(slug=self.kwargs['slug'])
+        return Photo.objects.filter(album_id=album_id)
+
+
+class TaggedList(ListView):
+    model = Photo
+    context_object_name = 'photos'
+
+    def get_queryset(self, *args, **kwargs):
+        return Photo.objects.filter(tags__slug=self.kwargs['tag'])
 
 
 def index(request):
