a76be70171f8657e295462be51ac520a92904c92
conformist-mw <conformist.mw@gmail.com>
2017-05-12 15:25:25 +0300

    new routes added

--- dachshunds/dachshunds/urls.py
+++ dachshunds/dachshunds/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('schatz.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
