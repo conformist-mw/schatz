e26ddee1f695ef307a8f21f13727c2adbbf267ea
conformist-mw <conformist.mw@gmail.com>
2017-05-13 14:50:19 +0300

    new routes added

--- dachshunds/schatz/urls.py
+++ dachshunds/schatz/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^breed/$', views.breed),
+    url(r'^puppies/$', views.puppies),
+    url(r'^articles/$', views.articles),
+    url(r'^contacts/$', views.contacts),
     url(r'^gallery/$', views.GalleryAlbums.as_view()),
     url(r'^gallery/tag/(?P<tag>.+)/$', views.TaggedList.as_view(), name='tagged'),
     url(r'^gallery/(?P<slug>.+)/$', views.PhotoGallery.as_view()),
